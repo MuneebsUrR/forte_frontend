ee6d81ffe6faf65b9e08e0ed1d8b4c874b8772c6
MuhammadTaimoorAnwar511 <anwartaimoor511@gmail.com>
2024-08-09 14:58:36 +0500

    fine

--- src/utils/logUtils.jsx
+++ src/utils/logUtils.jsx
@@ -1,20 +1,49 @@
+let userLogs = [];
+let logTimeout;
+
 export const logQuestionDetails = (candidateId, sqpId, qpId, startTime, currentSubjectIndex, currentQuestionIndex, selectedOptions, isAttempted, data) => {
   const currentSubject = data[currentSubjectIndex];
   const currentQuestion = currentSubject?.questions[currentQuestionIndex];
   const timeSpent = Date.now() - startTime;
 
   if (currentQuestion) {
-      const lastViewTime = new Date(startTime).toISOString();
-      const elapsedTime = new Date(startTime + timeSpent).toISOString();
+    const lastViewTime = new Date(startTime).toISOString();
+    const elapsedTime = new Date(startTime + timeSpent).toISOString();
 
-      console.log('candidateId:', candidateId);
-      console.log('sqpId:', sqpId);
-      console.log('qpId:', qpId);
-      console.log('IS_ATTEMPTED:', isAttempted);
-      console.log('QUESTION_ID:', currentQuestion.QUESTION_ID);
-      console.log('SELECTED_ANSWER:', selectedOptions[currentQuestionIndex] || '-1');
-      console.log('LAST_VIEW_TIME:', lastViewTime);
-      console.log('ELAPSED_TIME:', elapsedTime);
-      console.log('===========================================');
+    const logEntry = {
+      candidateId,
+      sqpId,
+      qpId,
+      isAttempted,
+      questionId: currentQuestion.QUESTION_ID,
+      selectedAnswer: selectedOptions[currentQuestionIndex] || '-1',
+      lastViewTime,
+      elapsedTime
+    };
+
+    userLogs.push(logEntry);
+
+    if (!logTimeout) {
+      logTimeout = setTimeout(() => {
+        if (userLogs.length > 0) {
+          userLogs.forEach(log => {
+            console.log('===========================================');
+            console.log('candidateId:', log.candidateId);
+            console.log('sqpId:', log.sqpId);
+            console.log('qpId:', log.qpId);
+            console.log('IS_ATTEMPTED:', log.isAttempted);
+            console.log('QUESTION_ID:', log.questionId);
+            console.log('SELECTED_ANSWER:', log.selectedAnswer);
+            console.log('LAST_VIEW_TIME:', log.lastViewTime);
+            console.log('ELAPSED_TIME:', log.elapsedTime);
+          });
+          console.log('===========================================');
+          // Reset the logs after displaying
+          userLogs = [];
+        }
+        logTimeout = null;
+      }, 60000); // 1 minute
+    }
   }
 };
+
